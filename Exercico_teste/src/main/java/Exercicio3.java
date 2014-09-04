64e786dced818e3935dc6a9ba45c812ed6cd1c40
LuandersonGomes <gomesluanderson@gmail.com>
2014-09-04 01:34:35 -0300

    Exercicio 3 com JOptionPane

--- Exercico_teste/src/main/java/Exercicio3.java
+++ Exercico_teste/src/main/java/Exercicio3.java
@@ -1,6 +1,20 @@
+import javax.swing.*;
+
 /**
  * Created by Luanderson on 04/09/2014.
  */
 public class Exercicio3
 {
+    public static void main(String[] Args)
+    {
+        Float[] numeros = new Float[10];
+        Float soma = new Float(0);
+
+        for (int contador = 0; contador <= 9; contador++)
+        {
+            numeros[contador] = Float.valueOf(Integer.parseInt(JOptionPane.showInputDialog("Digite Um Numero", null)));
+            soma = soma + numeros[contador];
+        }
+        JOptionPane.showMessageDialog(null,"O Valor da soma é: "+soma);
+    }
 }
